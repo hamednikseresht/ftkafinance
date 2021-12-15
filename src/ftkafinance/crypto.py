fa499e9644a3a09195f6cee0cb68f5da18156941
Ho3ein <hos.kh93@gmail.com>
2021-12-15 12:45:47 +0330

    convert all methods to classmethod & load_crypto_data to static method

--- src/ftkafinance/crypto.py
+++ src/ftkafinance/crypto.py
@@ -13,17 +13,17 @@ class Crypto:
     _df = pd.DataFrame()
     _symbol = ""
     _config = dotenv_values('/.env')
-
-    def set_symbol(self,symbol:str):
+    @classmethod
+    def set_symbol(cls,symbol:str):
         """ set the symbol of the class
         Parameters
         ----------
         symbol : str 
             symbol supported by Binance API.
         """
-        self._symbol = symbol
-    
-    def _progress(self,count, total, status=''):
+        cls._symbol = symbol
+    @classmethod
+    def _progress(cls,count, total, status=''):
         import sys
         """progress bar function developed by https://gist.github.com/vladignatyev
         parmeters: count, total, status
@@ -43,71 +43,79 @@ class Crypto:
 
         sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
         sys.stdout.flush()
-    
-    def _str_to_epoch_ms(self,date:str):
+    @classmethod
+    def _str_to_epoch_ms(cls,date:str):
         """ convert date string to epoch time in miliseconds """
         str_data = str(date)
         return int(datetime.strptime(str_data,'%Y-%m-%d').timestamp()*1000)
-
-    def _get_latest_data(self):
+    @classmethod
+    def _get_latest_data(cls):
         """Check the latest data inserted to mongoDB
         Returns:
             epoch time : last excist data in mongoDB
         """
-        client = self._mongo_connection()
-        mycol = client[self._config['MONGO_DB']][self._symbol]
+        client = cls._mongo_connection()
+        mycol = client[cls._config['MONGO_DB']][cls._symbol]
         latest_date = list(mycol.find({} , {"_id"}).sort("_id",-1).limit(1))
 
         return '1567965420000' if len(latest_date) == 0 else latest_date[0]["_id"]
-
-    def _symbols_list(self):
+    @classmethod
+    def _symbols_list(cls):
         """ return a list of binance valid symbols
         """
-        resp = requests.get(
-            url='https://fapi.binance.com/fapi/v1/exchangeInfo')
+        try:
+            resp = requests.get(
+                url='https://fapi.binance.com/fapi/v1/exchangeInfo')
+        except ConnectionError as ce:
+            sys.exit('Connection Error : {} \n'.format(ce))
+        except TimeoutError as te:
+            sys.exit('Timeout Error : {} \n'.format(te))
+        except:
+            sys.exit('\n*** Could not connect to API! ***'
+            + '\n*** Please check your internet connection ***')
         data = resp.json()
 
         # create a list of valid symbols
         symbol_list = [ s['symbol'].lower() for s in data['symbols']  ]
 
         return symbol_list
-
-    def _mongo_connection(self):
+    @classmethod
+    def _mongo_connection(cls):
         """ connect to mongoDB with credentials from .env file
         Returns:
             Mongo Client Oject
         """
         client = MongoClient(
-            self._config['MONGO_Host'],
-            int(self._config['MONGO_Port']),
-            username=self._config['MONGO_User'],
-            password=self._config['MONGO_Password'])
+            cls._config['MONGO_Host'],
+            int(cls._config['MONGO_Port']),
+            username=cls._config['MONGO_User'],
+            password=cls._config['MONGO_Password'])
         return client
-
-    def _insert_to_mongo(self):
+    @classmethod
+    def _insert_to_mongo(cls):
         """ insert data to mongoDB
             read .env file to get mongoDB credentials
             create a new collection if it doesn't exist
         """
-        client = self._mongo_connection() 
+        client = cls._mongo_connection() 
         try :
             client
         except Exception as exp:
             raise SystemExit(exp)
             
-        mycol = client[self._config['MONGO_DB']][self._symbol]
+        mycol = client[cls._config['MONGO_DB']][cls._symbol]
         # Set the unique index using the open time
-        self._df['_id']=self._df.OpenTime.astype(str)
+        cls._df['_id']=cls._df.OpenTime.astype(str)
         # First convert the dataframe to a list of dictionaries
         # then insert the list into the collection with json format 
         try:
             # Ignore duplicate records
-            json_list = json.loads(json.dumps(list(self._df.T.to_dict().values())))
+            json_list = json.loads(json.dumps(list(cls._df.T.to_dict().values())))
             mycol.insert_many(json_list , ordered=False)
         except Exception as exp:
             pass
-    
-    def _tf_maker(self,data,interval):
+    @classmethod
+    def _tf_maker(cls,data,interval):
         """ transform data to desire time frequency 
             with candle stick and volume data pattern
         """
@@ -137,8 +145,8 @@ class Crypto:
             sys.exit(print(offset))
             
         return data
-    
-    def _clean_data (self,data):
+    @classmethod
+    def _clean_data (cls,data):
         """transform data in data frame to classic candle stick with volume
         Parameters
         ----------
@@ -158,8 +166,8 @@ class Crypto:
         data['Volume'] = pd.to_numeric(data['Volume'],downcast="float")
         
         return data[['Open','High','Low','Close','Volume']]
-
-    def _data_update (self,data_frame):
+    @classmethod
+    def _data_update (cls,data_frame):
         """ update today's data untile now
         Parameters
         ----------
@@ -170,11 +178,11 @@ class Crypto:
         """
         start = date.today()
         end = start + timedelta(days=1)
-        data_frame = data_frame.append(self._data_to_df(start.isoformat(),
+        data_frame = data_frame.append(cls._data_to_df(start.isoformat(),
                                                 end.isoformat()))
         return data_frame
-
-    def _data_to_df (self,start_time:str,end_time:str):
+    @classmethod
+    def _data_to_df (cls,start_time:str,end_time:str):
         """ Get data from Binance API and convert it to a pandas data frame
         numbers of candles are limited to 1500 per request
         Parameters
@@ -194,15 +202,15 @@ class Crypto:
         df : pandas data frame
             converted API response to a data frame.
         """
-        start = self._str_to_epoch_ms(start_time)
-        end = self._str_to_epoch_ms(end_time)
+        start = cls._str_to_epoch_ms(start_time)
+        end = cls._str_to_epoch_ms(end_time)
 
         if end<start:
             raise SystemExit("End date must be larger than start date")
             
         url = 'https://fapi.binance.com/fapi/v1/klines'
         params = {
-        'symbol':str(self._symbol),
+        'symbol':str(cls._symbol),
         'interval':'1m',
         'startTime':start,
         'endTime': end,
@@ -212,7 +220,6 @@ class Crypto:
             request = requests.get(url,params=params)
         except requests.exceptions.RequestException as exp :
             raise SystemExit(exp)
-            sys.exit(1)
         
         data = request.json()
         if type(data) == dict:
@@ -232,8 +239,8 @@ class Crypto:
         df.index = df.OpenTime
         
         return df
-
-    def _collect_data (self, start_date:str, end_date:str):
+    @classmethod
+    def _collect_data (cls, start_date:str, end_date:str):
         """ Split request with large interval time to smaller request in
         order to over come binance api limitaion 
         Parameters
@@ -246,26 +253,27 @@ class Crypto:
         date_range = pd.date_range(start_date,end_date)
         for i,day in enumerate(date_range):
             if len(date_range)>i+1:
-                    self._df = self._df.append(
-                            self._data_to_df(day.strftime('%Y-%m-%d'),
+                    cls._df = cls._df.append(
+                            cls._data_to_df(day.strftime('%Y-%m-%d'),
                                         date_range[i+1].strftime('%Y-%m-%d')))
-                    self._progress(i,len(date_range),status=f"{self._symbol} data is loading")
+                    cls._progress(i,len(date_range),status=f"{cls._symbol} data is loading")
             if i % 50 == 0:
                 time.sleep(2)
-
-    def _get_data(self):
+    @classmethod
+    def _get_data(cls):
         """check if data is already in mongoDB or not
             in case of not, collect data from binance api until the end of yesterday 
             and insert it to mongoDB 
         """
         # check latest data in mongoDB -100 is for avoiding confussion in time 00:00:00
-        latest = date.fromtimestamp((int(self._get_latest_data()) / 1000)-100)
+        latest = date.fromtimestamp((int(cls._get_latest_data()) / 1000)-100)
         yesterday = date.today() - timedelta(days=1)
         if(latest != yesterday):
-            self._collect_data(str(latest) , str(date.today()))
-            self._insert_to_mongo()
-
-    def load_crypto_data(self,symbol,start_date='2019-01-01',
+            cls._collect_data(str(latest) , str(date.today()))
+            cls._insert_to_mongo()
+    
+    @staticmethod
+    def load_crypto_data(symbol,start_date='2019-01-01',
                 end_date=str(date.today()),
                 interval='1T'):
         """load data from mongoDB and convert it to a pandas data frame
@@ -282,26 +290,26 @@ class Crypto:
                 default value is 1T (1 minute)
         """
         # check if input symbol is valid
-        if symbol.lower() not in self._symbols_list():
+        if symbol.lower() not in Crypto._symbols_list():
             raise SystemExit("Symbol is not valid \n\
-                Please select correct symbol: \n{}".format(self._symbols_list()))
+                Please select correct symbol: \n{}".format(Crypto._symbols_list()))
 
-        self.set_symbol(symbol)
-        client = self._mongo_connection()
-        mycol = client[self._config['MONGO_DB']][self._symbol]
+        Crypto.set_symbol(symbol)
+        client = Crypto._mongo_connection()
+        mycol = client[Crypto._config['MONGO_DB']][Crypto._symbol]
         try :
             mycol
         except Exception as exp:
             raise SystemExit(exp)
 
-        start = self._str_to_epoch_ms(start_date)
-        end = self._str_to_epoch_ms(end_date)
+        start = Crypto._str_to_epoch_ms(start_date)
+        end = Crypto._str_to_epoch_ms(end_date)
 
-        latest = int(self._get_latest_data())
+        latest = int(Crypto._get_latest_data())
 
         # check data exist in database
         if end > latest :
-            self._get_data()
+            Crypto._get_data()
 
         # get interval data from mongoDB and convert it to a pandas data frame
         data = pd.DataFrame(list(mycol.find(
@@ -309,12 +317,12 @@ class Crypto:
                     '$lt':str(end)}})))
         
         # today data , append to database fetched data
-        if end >= self._str_to_epoch_ms(str(date.today())) :       
-            data = self._data_update(data)
+        if end >= Crypto._str_to_epoch_ms(str(date.today())) :       
+            data = Crypto._data_update(data)
         
-        if data.empty == True:
+        if data.empty:
             raise SystemExit("There is no valid data for this interval")
 
-        data = self._clean_data(data)
+        data = Crypto._clean_data(data)
         
-        return self._tf_maker(data,interval)
+        return Crypto._tf_maker(data,interval)
